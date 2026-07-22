fb83dd68983e44f7d5c09b4f42d48adc50a9be2a
Ye Yu <yeyuqwer@gmail.com>
2026-07-22 10:40:40 +0800

    feat(kitty): refine cursor, fonts, and theme syncing

--- .config/kitty/sync-starship-theme.sh
+++ .config/kitty/sync-starship-theme.sh
@@ -5,7 +5,7 @@ stateFile="${kittyConfigDir}/.theme-state"
 starshipConfigFile="${HOME}/.config/starship.toml"
 
 if [ -f "${stateFile}" ] && [ "$(cat "${stateFile}")" = "light" ]; then
-    cp "${kittyConfigDir}/starship-light.toml" "${starshipConfigFile}"
+    perl -pi -e 's/#7AA2F7/#2E7DE9/g; s/#7DCFFF/#007197/g; s/#BB9AF7/#9854F1/g; s/#9D7CD8/#7847BD/g; s/#9ECE6A/#587539/g' "${starshipConfigFile}"
 else
-    cp "${kittyConfigDir}/starship-dark.toml" "${starshipConfigFile}"
+    perl -pi -e 's/#2E7DE9/#7AA2F7/g; s/#007197/#7DCFFF/g; s/#9854F1/#BB9AF7/g; s/#7847BD/#9D7CD8/g; s/#587539/#9ECE6A/g' "${starshipConfigFile}"
 fi
